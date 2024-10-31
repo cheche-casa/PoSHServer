60a7b5f3e560aedd9b6bc2cb446c85213a6870b5
cheche.casa <cheche.casa@outlook.es>
2024-10-31 08:55:32 +0100

    Para ler o último seria

---
+++ serial.ps1
@@ -0,0 +1,20 @@
+$conn = New-Object System.Data.OleDb.OleDbConnection;
+$conn.ConnectionString= "Provider=SQLOLEDB;Data Source=fuegovm.rpso.usc.es;Initial Catalog=SQLHealth;User Id=probas;Password=N0cturn1dade;";
+$cmd = New-Object System.Data.OleDb.OleDbCommand;
+$cmd.Connection = $conn;
+try {
+    $conn.open();
+}
+catch {
+    Write-Output "$Cerro-$($Error[0].ToString())";
+}
+
+$Query = "SELECT IDENT_CURRENT('comandos') AS Serial;";
+$cmd.CommandText = $Query;
+$Serial = $cmd.ExecuteScalar();
+$Resultado = [PSCustomObject]@{
+    serial = $Serial
+}
+$Saida = $Resultado|ConvertTo-Json;
+Write-Output $Saida;
+$conn.close();
