b4c6c862b0b6f387d4a76a9e299d247956ac52cf
cheche.casa <cheche.casa@outlook.es>
2025-01-21 21:22:49 +0100

    nova páxina

---
+++ versionU.ps1
@@ -0,0 +1,28 @@
+$conn = New-Object System.Data.OleDb.OleDbConnection;
+$conn.ConnectionString = "Provider=SQLOLEDB;Data Source=pccheche;Initial Catalog=Merca;Integrated Security=SSPI;"
+$cmd = New-Object System.Data.OleDb.OleDbCommand;
+$cmd.Connection = $conn;
+try {
+    $conn.open();
+}
+catch {
+    Write-Output "$Cerro-$($Error[0].ToString())";
+}
+
+$U = $PoSHQuery.U;
+$Query = "SELECT Version FROM USUARIOS WHERE Id ='$U';";
+$cmd.CommandText = $Query;
+try {
+    $Version = $cmd.ExecuteScalar();
+    $Resultado = [PSCustomObject]@{
+        version = $Version
+    }
+}
+Catch {
+    $Resultado = [PSCustomObject]@{
+        version = 0
+    }
+}
+$Saida = $Resultado|ConvertTo-Json;
+Write-Output $Saida;
+$conn.close();
